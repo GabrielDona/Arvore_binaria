798be1dbb66b244fd8f4db9c983287f0ea515551
Gabriel Doná <gabrieldona2004@gmail.com>
2025-05-20 21:54:37 -0300

    contador de Nós

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,4 +1,12 @@
 public class ArvoreBinaria {
     No raiz;
 
+    public int contadorNo(No raiz){
+        if (raiz == null){
+            return 0;
+        } else {
+            return 1 + contadorNo(raiz.esquerdo) + contadorNo(raiz.direito);
+        }
+    }
+
 }
